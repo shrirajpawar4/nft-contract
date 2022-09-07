c76c561460a49a65e1acd6c9c7ded156f551405b
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-09-08 00:04:52 +0530

    working :hammer:

--- smart-contracts/contracts/NFT.sol
+++ smart-contracts/contracts/NFT.sol
@@ -10,8 +10,65 @@ contract NFT is ERC721Enumerable, Ownable {
 
     IWhitelist whitelist;
 
+    bool public presaleStarted;
+
+    uint256 public presaleEnded;
+
+    uint256 public maxTokenId = 20;
+
+    uint256 public tokenId;
+
+    uint256 public _price = 0.05 ether; 
+
+    bool public _paused;
+
     constructor(string memory _baseURI, address whitelistContract) ERC721("Shree's collection", "SHR") {
         _baseTokenURI = _baseURI;
         whitelist = IWhitelist(whitelistContract);
     }  
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMint() public payable {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale has ended");
+        require(whitelist.whiteListAddresses(msg.sender), "Address not in whitelist");
+        require(tokenId < maxTokenId, "Limit Exceeded");
+        require(msg.value >= _price, "Ether is too low");
+
+        tokenId += 1;
+
+        _safeMint(msg.sender, tokenId); 
+
+    }
+
+    function mint() public payable {
+        require(presaleStarted && block.timestamp >= presaleEnded, "Presale still on");
+        require(tokenId < maxTokenId, "Limit Exceeded");
+        require(msg.value >= _price, "Ether is too low");
+
+        tokenId += 1;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function _baseURI() internal view override returns(string memory) {
+        return _baseTokenURI;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        //boolean to check if ether is sent or not. amount from contract is given to owner(address who calls contract)
+        (bool sent, ) = _owner.call{value: amount}(" ");
+        require(sent, "Failed to send ether");
+
+
+    }
+
+    receive() external payable {}  
+
+    fallback() external payable {}
+
 }
