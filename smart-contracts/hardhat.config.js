96dece9c2741cd2781ef7d405d251e3d139a29aa
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-09-10 19:42:15 +0530

    deploy js and mumbai testnet :spark:

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const {ALCHEMY_API_URL, POLYGON_PRIVATE_KEY} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${POLYGON_PRIVATE_KEY}`]
+    }
+  }
 };
